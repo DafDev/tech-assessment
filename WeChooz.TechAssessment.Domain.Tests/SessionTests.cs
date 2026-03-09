4237546404ee83af4a5ad5b9ebdb1017c78c07cf
Daf <dafDev@gmail.com>
2026-03-09 10:16:52 +0100

    feat(domain): add test to cover that a participant is correctly added regarding max capacity

--- WeChooz.TechAssessment.Domain.Tests/SessionTests.cs
+++ WeChooz.TechAssessment.Domain.Tests/SessionTests.cs
@@ -2,12 +2,13 @@
 
 public class SessionTests
 {
+    private readonly Person _instructor = new Person("Jean", "Biche");
     [Fact]
     public void GivenSessionWithAvailableSeats_WhenAddingAttendant_ThenAttendantIsAdded()
     {
         // Arrange
-        var course = new Course("Course Title", "Short Description", 5, 3, TargetDemographic.ElectedMember, new Person("John", "Doe"));
-        var session = new Session(course, new DateTimeOffset(2026, 3, 15));
+        var course = new Course("Course Title", "Short Description", 5, 3, TargetDemographic.ElectedMember, _instructor);
+        var session = new Session(course, new DateTimeOffset(new DateTime(2026, 3, 15)));
         var attendant = new Attendant(new Person("Jane", "Smith"), TargetDemographic.ElectedMember);
 
         // Act
@@ -15,5 +16,25 @@ public class SessionTests
 
         // Assert
         Assert.Contains(attendant, session.Attendants);
+    }
+
+    [Fact]
+    public void GivenSessionWithNoAvailableSeats_WhenAddingAttendant_ThenAttendantIsNotAdded()
+    {
+        // Arrange
+        List<Attendant> initialAttendants = 
+        [
+            new Attendant(new Person("Jane", "Smith"), TargetDemographic.ElectedMember),
+            new Attendant(new Person("John", "Doe"), TargetDemographic.ElectedMember)
+        ];
+        var course = new Course("Course Title", "Short Description", 5, 2, TargetDemographic.ElectedMember, _instructor);
+        var session = new Session(course, new DateTimeOffset(new DateTime(2026, 3, 15)), initialAttendants);
+        var attendantAlice = new Attendant(new Person("Alice", "Johnson"), TargetDemographic.ElectedMember);
+
+        // Act
+        session.AddAttendant(attendantAlice);
+
+        // Assert
+        Assert.DoesNotContain(attendantAlice, session.Attendants);
     }
 }
