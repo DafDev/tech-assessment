938646b10b98d160285756599d358a5ea1118d13
Daf <dafDev@gmail.com>
2026-03-10 11:55:14 +0100

    fix(tests): update SessionTestsData to include longDescription in Course constructor

--- WeChooz.TechAssessment.Domain.Tests/SessionTestsData.cs
+++ WeChooz.TechAssessment.Domain.Tests/SessionTestsData.cs
@@ -5,12 +5,12 @@ public class SessionTestsData : TheoryData<Session, Attendant, bool>
     public SessionTestsData()
     {
         var durationInDays = 5;
-        var courseWithAvailableSeats = new Course("Basics of Rock", "Short Description", durationInDays, 3, TargetDemographic.ElectedMember, new Person("Chuck", "Berry"));
-        var courseWithNoAvailableSeats = new Course("Elvis was a fraud (kinda)", "I mean...", durationInDays, 2, TargetDemographic.ElectedMember, new Person("Little", "Richard"));
-        var courseForEveryoneAddPresident = new Course("How to sing beautifully", "Bring me to life", durationInDays, 5, TargetDemographic.ElectedMember, new Person("Amy", "Lee"));
-        var courseForPresidentAddPresident = new Course("How to scream like a demon", "Demonic growl", durationInDays, 5, TargetDemographic.President, new Person("Ryo", "Kinoshita"));
-        var courseForPresidentAddElectedMember = new Course("How to scream like a possessed person", "Demonic growl", durationInDays, 5, TargetDemographic.President, new Person("Corey", "Taylor"));
-        var courseWithAttendantAlreadyIInIt = new Course("Metal can be fun", "It's not all doom and gloom", durationInDays, 5, TargetDemographic.ElectedMember, new Person("Kevin", "Ratajczak"));
+        var courseWithAvailableSeats = new Course("Basics of Rock", "Short Description", "A comprehensive introduction to rock music fundamentals", durationInDays, 3, TargetDemographic.ElectedMember, new Person("Chuck", "Berry"));
+        var courseWithNoAvailableSeats = new Course("Elvis was a fraud (kinda)", "I mean...", "In fairness, he did popularize rock and roll for mainstream audiences", durationInDays, 2, TargetDemographic.ElectedMember, new Person("Little", "Richard"));
+        var courseForEveryoneAddPresident = new Course("How to sing beautifully", "Bring me to life", "A deep dive into vocal techniques for all skill levels", durationInDays, 5, TargetDemographic.ElectedMember, new Person("Amy", "Lee"));
+        var courseForPresidentAddPresident = new Course("How to scream like a demon", "Demonic growl", "Advanced extreme vocal techniques reserved for presidents", durationInDays, 5, TargetDemographic.President, new Person("Ryo", "Kinoshita"));
+        var courseForPresidentAddElectedMember = new Course("How to scream like a possessed person", "Demonic growl", "Mastering the art of extreme vocals exclusively for presidents", durationInDays, 5, TargetDemographic.President, new Person("Corey", "Taylor"));
+        var courseWithAttendantAlreadyIInIt = new Course("Metal can be fun", "It's not all doom and gloom", "Exploring the lighter and more accessible side of heavy metal", durationInDays, 5, TargetDemographic.ElectedMember, new Person("Kevin", "Ratajczak"));
         var date = new DateTimeOffset(new DateTime(2026, 3, 15));
 
         Add(
